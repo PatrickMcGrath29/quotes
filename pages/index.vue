851cef373f04a23c6c8e40c75199e6a9ef5195e0
Patrick McGrath <patrickmcgrath29@gmail.com>
2022-12-10 13:59:25 -0500

    tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,19 @@
 <template>
   <Container>
-    <div class="flex gap-3 flex-wrap">
+    <div class="grid gap-x-3 gap-y-8 grid-cols-1 md:grid-cols-3">
       <div>
-        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
+        <div class="card bg-primary hover:bg-opacity-80 bg-base-100 shadow-xl">
           <div class="card-body">
             <p>
               We all know the value of joy. It alone is the proof that what we
               have really satisfies the heart.
             </p>
-            <h4 class="card-title">Andrew Murray</h4>
+            <h4 class="card-title text-lg">Andrew Murray</h4>
           </div>
         </div>
       </div>
-      <div>
-        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
-          <div class="card-body">
-            <p>
-              There is no proof of the reality of God’s love and the blessing He
-              bestows, which men so soon feel the force of, as when the joy of
-              God overcomes all the trials of life.
-            </p>
-            <h4 class="card-title">Andrew Murray</h4>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
-          <div class="card-body">
-            <p>
-              With a heart full of joy no work can weary, and no burden can
-              depress; God Himself is strength and song.
-            </p>
-            <h4 class="card-title">Andrew Murray</h4>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
-          <div class="card-body">
-            <p>
-              I do not pray that you may be delivered from your pains; but I
-              pray GOD earnestly that He would give you strength and patience to
-              bear them as long as He pleases. Comfort yourself with Him who
-              holds you fastened to the cross: He will loose you when He thinks
-              fit. Happy those who suffer with Him: accustom yourself to suffer
-              in that manner, and seek from Him the strength to endure as much,
-              and as long, as He shall judge to be necessary for you.
-            </p>
-            <h4 class="card-title">Brother Lawrence</h4>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
+      <div class="col-span-2">
+        <div class="card bg-primary hover:bg-opacity-80 bg-base-100 shadow-xl">
           <div class="card-body">
             <p>
               In our desire after God, let us keep always in mind that God also
@@ -64,7 +25,46 @@
               us in Christ Jesus. With them God can walk unhindered, toward them
               He can act like the God He is.
             </p>
-            <h4 class="card-title">A.W. Tozer</h4>
+            <h4 class="card-title text-lg">A.W. Tozer</h4>
+          </div>
+        </div>
+      </div>
+      <div class="col-span-2">
+        <div class="card bg-primary hover:bg-opacity-80 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              I do not pray that you may be delivered from your pains; but I
+              pray GOD earnestly that He would give you strength and patience to
+              bear them as long as He pleases. Comfort yourself with Him who
+              holds you fastened to the cross: He will loose you when He thinks
+              fit. Happy those who suffer with Him: accustom yourself to suffer
+              in that manner, and seek from Him the strength to endure as much,
+              and as long, as He shall judge to be necessary for you.
+            </p>
+            <h4 class="card-title text-lg">Brother Lawrence</h4>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="card bg-primary hover:bg-opacity-80 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              With a heart full of joy no work can weary, and no burden can
+              depress; God Himself is strength and song.
+            </p>
+            <h4 class="card-title text-lg">Andrew Murray</h4>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="card bg-primary hover:bg-opacity-80 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              There is no proof of the reality of God’s love and the blessing He
+              bestows, which men so soon feel the force of, as when the joy of
+              God overcomes all the trials of life.
+            </p>
+            <h4 class="card-title text-lg">Andrew Murray</h4>
           </div>
         </div>
       </div>
