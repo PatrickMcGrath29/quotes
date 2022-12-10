89b8e4ea43338471b4a58e341f8965ad05ec8a61
Patrick McGrath <patrickmcgrath29@gmail.com>
2022-12-10 13:53:07 -0500

    updates

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <Container>
+      <div class="p-10">
+        <h1 class="text-3xl">Quotes</h1>
+      </div>
+    </Container>
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,71 @@
 <template>
   <Container>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <p>Quote Text Here</p>
-          <h4 class="card-title">Andrew Murray</h4>
+    <div class="flex gap-3 flex-wrap">
+      <div>
+        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              We all know the value of joy. It alone is the proof that what we
+              have really satisfies the heart.
+            </p>
+            <h4 class="card-title">Andrew Murray</h4>
+          </div>
         </div>
       </div>
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <p>Quote Text Here</p>
-          <h4 class="card-title">Andrew Murray</h4>
+      <div>
+        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              There is no proof of the reality of God’s love and the blessing He
+              bestows, which men so soon feel the force of, as when the joy of
+              God overcomes all the trials of life.
+            </p>
+            <h4 class="card-title">Andrew Murray</h4>
+          </div>
         </div>
       </div>
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <p>Quote Text Here</p>
-          <h4 class="card-title">Andrew Murray</h4>
+      <div>
+        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              With a heart full of joy no work can weary, and no burden can
+              depress; God Himself is strength and song.
+            </p>
+            <h4 class="card-title">Andrew Murray</h4>
+          </div>
         </div>
       </div>
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <p>Quote Text Here</p>
-          <h4 class="card-title">Andrew Murray</h4>
+      <div>
+        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              I do not pray that you may be delivered from your pains; but I
+              pray GOD earnestly that He would give you strength and patience to
+              bear them as long as He pleases. Comfort yourself with Him who
+              holds you fastened to the cross: He will loose you when He thinks
+              fit. Happy those who suffer with Him: accustom yourself to suffer
+              in that manner, and seek from Him the strength to endure as much,
+              and as long, as He shall judge to be necessary for you.
+            </p>
+            <h4 class="card-title">Brother Lawrence</h4>
+          </div>
         </div>
       </div>
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <p>Quote Text Here</p>
-          <h4 class="card-title">Andrew Murray</h4>
+      <div>
+        <div class="card bg-primary w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              In our desire after God, let us keep always in mind that God also
+              has a desire, and His desire is toward the sons of men, and more
+              particularly toward those sons of men who will make the
+              once-for-all decision to exalt Him over all. Such as these are
+              precious to God above all treasures of earth or sea. In them God
+              finds a theater where He can display His exceeding kindness toward
+              us in Christ Jesus. With them God can walk unhindered, toward them
+              He can act like the God He is.
+            </p>
+            <h4 class="card-title">A.W. Tozer</h4>
+          </div>
         </div>
       </div>
     </div>
