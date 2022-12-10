4022be57349053bf6d93cec0704352f0afb45a1b
Patrick McGrath <patrickmcgrath29@gmail.com>
2022-12-10 14:22:20 -0500

    tweak

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@ const quotes = await queryContent("quotes").findOne();
 <template>
   <Container>
     <div class="grid gap-7 grid-cols-1 md:grid-cols-3">
-      <div v-for="quote in quotes.body" :class="`col-span-${quote.size}`">
+      <div v-for="quote in quotes.body">
         <div class="card bg-primary hover:bg-opacity-80 shadow-xl">
           <div class="card-body">
             <p>
