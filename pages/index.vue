0d52f8f5461163aa833dda6a0c534b5632789610
Patrick McGrath <patrickmcgrath29@gmail.com>
2022-12-11 15:43:36 -0500

    update

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,11 @@ const quotes = await queryContent("quotes").findOne();
 
 <template>
   <Container>
-    <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4">
-      <div v-for="quote in quotes.body" :class="`col-span-${quote.size}`">
+    <div class="grid grid-cols-1 md:grid-cols-3 grid-flow-row-dense gap-4">
+      <div
+        v-for="quote in quotes.body"
+        :class="`max-md:col-auto col-span-${quote.size}`"
+      >
         <div class="bg-neutral hover:bg-opacity-80 shadow-xl rounded-md">
           <div class="card-body">
             <p>
