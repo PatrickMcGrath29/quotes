6b1939746d68ab371ea253150a770a11e040f5c6
Patrick McGrath <patrickmcgrath29@gmail.com>
2023-04-03 19:41:12 -0400

    Dynamically compute size

--- composables/quotes.ts
+++ composables/quotes.ts
@@ -1,9 +1,22 @@
-import { Quote, Quotes } from "~/types";
+import { Quote, RawQuotes } from "~/types";
+
+const computeSize = (text: string): Number => {
+  return Math.ceil(text.length / 250);
+};
+
+const toQuote = (content: any): Quote => {
+  return {
+    text: content.text,
+    reference: content.reference,
+    size: computeSize(content.text),
+    author: content.author,
+  };
+};
 
 export const useQuotes = async (): Promise<Quote[]> => {
   const { data } = await useAsyncData(() =>
-    queryContent<Quotes>("quotes").findOne()
+    queryContent<RawQuotes>("quotes").findOne()
   );
 
-  return data.value?.quotes;
+  return data.value?.quotes.map(toQuote);
 };
--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,18 @@
 import type { ParsedContent } from "@nuxt/content/dist/runtime/types";
 
+export interface RawQuotes extends ParsedContent {
+  quotes: RawQuote[];
+}
+
+export interface RawQuote {
+  text: string;
+  author: string;
+  reference: string;
+}
+
 export interface Quote {
   text: string;
   author: string;
   reference: string;
   size: Number;
-}
-
-export interface Quotes extends ParsedContent {
-  quotes: Quote[];
 }
