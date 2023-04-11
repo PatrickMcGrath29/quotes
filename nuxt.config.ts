7b5912b66b9c094c134862bd8a671f302b58b392
Patrick McGrath <patrickmcgrath29@gmail.com>
2023-04-10 22:37:46 -0400

    tweak

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "Quotes, for the body of Christ",
+      title: "Quotes for the body of Christ",
     },
   },
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@vueuse/nuxt"],
