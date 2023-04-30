54bef6abd0b852179a0c21833a84a022351b0c7e
Patrick McGrath <patrickmcgrath29@gmail.com>
2023-04-29 20:49:22 -0400

    Another update

--- pages/quote/[id].vue
+++ pages/quote/[id].vue
@@ -19,7 +19,7 @@ const quote = computed(() => {
     <hr />
     {{ route.params.id }}
     <hr />
-    {{ quoteStore.quotes.find((q: Quote) => q.uuid == route.params.id) }}
+    {{ quoteStore.quotes.filter((q: Quote) => q.uuid == route.params.id) }}
     <hr />
     {{ quote }}
     <div class="flex items-center flex-col" v-if="quote">
