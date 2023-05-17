6918cf5e7e5b5003bc3c8a7f370e62955fbe8b53
Patrick McGrath <patrickmcgrath29@gmail.com>
2023-05-17 14:59:11 -0400

    Upgrade nuxt version

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@ const columnSettings = computed(() => {
     <div class="gap-6 mt-4 mb-10" :class="columnSettings">
       <QuoteCard
         v-for="quote in quoteStore.filteredQuotes"
-        class="hover:brightness-125 transition-all duration-300"
+        class="hover:brightness-125 transition-all duration-300 select-text"
         :quote="quote"
       />
     </div>
