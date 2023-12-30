acd38319a6b7b62dd65430ca6bd0d00d6f2ea325
Patrick McGrath <patrickmcgrath29@gmail.com>
2023-12-30 09:39:57 +0100

    Add about page

--- components/header/HeaderItems.vue
+++ components/header/HeaderItems.vue
@@ -1,3 +1,8 @@
 <template>
   <li><a href="https://thegoodland.io" class="text-base">Blog</a></li>
+  <li>
+    <NuxtLink to="/about" class="text-base">
+      About
+    </NuxtLink>
+  </li>
 </template>
---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <Container>
+    <StyledCard>
+      <h1 class="text-xl mb-7 text-center font-semibold">
+        About
+      </h1>
+      <div>
+        God has revealed tremendous riches about who He is to faithful men and
+        women of the past. This website aims to make some of these riches known to
+        more of the body of Christ. May we find encouragement from the saints of
+        the past, and then press on even further in our walk with Christ.
+      </div>
+      <div class="mt-4">
+        <div class="mb-1 text-center italic">
+          "... reaching forward to those things which are ahead, I press toward
+          the goal for the prize of the upward call of God in Christ Jesus"
+        </div>
+        <div class="font-medium text-center">
+          Philippians 3:13-14
+        </div>
+      </div>
+    </StyledCard>
+  </Container>
+</template>
